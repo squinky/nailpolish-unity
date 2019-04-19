08fce9ce5305924e3a6ada6925aada21c2ac00dc
Squinky <hey@squinky.me>
2019-04-19 16:06:04 -0400

    camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField, Range(0.0f, 1.0f)]
+	private float _lerpRate;
+	private float _xRotation;
+	private float _yYRotation;
+ 
+	private void Rotate(float xMovement, float yMovement)
+	{
+		_xRotation += xMovement;
+		_yYRotation += yMovement;
+	}
+ 
+	void Start ()
+	{
+		InputManager.MouseMoved += Rotate;
+	}
+ 
+	void Update ()
+	{
+		_xRotation = Mathf.Lerp(_xRotation, 0, _lerpRate);
+		_yYRotation = Mathf.Lerp(_yYRotation, 0, _lerpRate);
+		transform.eulerAngles += new Vector3(_yYRotation, -_xRotation, 0);
+	}
+ 
+	void OnDestroy()
+	{
+	   InputManager.MouseMoved -= Rotate;    
+	}
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void MouseMoved(float xMovement, float yMovement);
+public class InputManager : MonoBehaviour
+{
+	private float _xMovement;
+	private float _yMovement;
+
+	public static event MouseMoved MouseMoved;
+
+	private static void OnMouseMoved(float xmovement, float ymovement)
+	{
+		var handler = MouseMoved;
+		if (handler != null) handler(xmovement, ymovement);
+	}
+
+	private void InvokeActionOnInput()
+	{
+		_xMovement = Input.GetAxis("Mouse X");
+		_yMovement = Input.GetAxis("Mouse Y");
+		OnMouseMoved(_xMovement, _yMovement);
+	}
+ 
+	void Update()
+	{
+		InvokeActionOnInput();
+	}
+}
